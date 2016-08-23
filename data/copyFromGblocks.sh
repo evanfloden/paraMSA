ac306ddaea9d4737d6de16105637a0d7aebf9e6d
skptic <evanfloden@gmail.com>
2016-08-23 14:38:36 +0200

    data clean up

--- data/copyFromGblocks.sh
+++ data/copyFromGblocks.sh
@@ -12,10 +12,11 @@ do
     for i in {001..100}
     do
 
-     esl-reformat afa /users/cn/jchang/projects/2010-03_ConM-Coffee/results/2013-03-26_bench-simulation/data/tips${tip}/asymmetric_${sym}/${i}/seqs/${i}.0400.fa > tips${tip}_${sym}/tips${tip}_${sym}_${i}.0400.afa
+     #esl-reformat afa /users/cn/jchang/projects/2010-03_ConM-Coffee/results/2013-03-26_bench-simulation/data/tips${tip}/symmetric_${sym}/${i}/seqs/${i}.0400.fa > tips${tip}_${sym}/tips${tip}_${sym}_${i}.0400.afa
 
-     t_coffee -other_pg seq_reformat -in /users/cn/jchang/projects/2010-03_ConM-Coffee/results/2013-03-26_bench-simulation/data/tips${tip}/asymmetric_${sym}/${i}/seqs/${i}.0400.fa -output fasta_seq > tips${tip}_${sym}/tips${tip}_${sym}_${i}.0400.fa
-
+     #t_coffee -other_pg seq_reformat -in /users/cn/jchang/projects/2010-03_ConM-Coffee/results/2013-03-26_bench-simulation/data/tips${tip}/symmetric_${sym}/${i}/seqs/${i}.0400.fa -output fasta_seq > tips${tip}_${sym}/tips${tip}_${sym}_${i}.0400.fa
+   
+    cp /users/cn/jchang/projects/2010-03_ConM-Coffee/results/2013-03-26_bench-simulation/data/tips${tip}/symmetric_${sym}/${i}/seqs/${i}.0400.fa tips${tip}_${sym}/tips${tip}_${sym}_${i}.0400.fa
 
     done
   done
